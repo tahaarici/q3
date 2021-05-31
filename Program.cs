80b2357deeb9ea4110e27843c361e52c32210efa
Taha Arıcı <79743901+tahaarici@users.noreply.github.com>
2021-05-31 12:51:11 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+public class SwapExample
+{
+    public static void Main(string[] args)
+    {
+        int a = 3, b = 6;
+        Console.WriteLine("Before swap a=" + a + "b=" + b);
+        a = a*b; //a=18 (3*6)
+        b = a/b;  //b=3 (18/6)
+        a = a/b; //a=6 (18/3)
+        Console.WriteLine("After swap a=" + a + "b=" + b);
+    }
+
+}
